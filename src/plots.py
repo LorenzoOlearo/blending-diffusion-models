035ba2e4b332550e7a1f808a1bdfdad794a2edc2
Lorenzo Olearo <lorenzo.olearo@olearo.com>
2024-05-01 16:10:58 +0000

    Rework plots

--- src/plots.py
+++ src/plots.py
@@ -4,12 +4,12 @@ from PIL import Image
 import numpy as np
 import os
 
-FINAL_IMAGE_PREFIX = "final-"
-FINAL_IMAGE_BLEND_PREFIX = "blend-"
-INTERMEDIATE_IMAGE_PREFIX = "intermediate-"
+
+FINAL_IMAGE_PREFIX = "final"
+FINAL_IMAGE_BLEND_PREFIX = "blend"
+INTERMEDIATE_IMAGE_PREFIX = "intermediate"
 
 
-# TODO: refactor considering the new config structure and the different methods of blending
 def save_all_outputs(config, prompt_1_images, prompt_2_images, blend_images, blend_method, output_path):
     prompt_1 = config["prompt_1"]
     prompt_2 = config["prompt_2"]
@@ -17,13 +17,15 @@ def save_all_outputs(config, prompt_1_images, prompt_2_images, blend_images, ble
     final_image_1 = prompt_1_images[-1]
     final_image_2 = prompt_2_images[-1]
     final_image_blend = blend_images[-1]
-    intermediate_image_blend = prompt_1_images[config["from_timestep"]]
    
     additional_parameters = get_additional_parameters_string(config, blend_method)
     
     final_image_1.save(f"{output_path}/{FINAL_IMAGE_PREFIX}-{prompt_1}.png")
     final_image_2.save(f"{output_path}/{FINAL_IMAGE_PREFIX}-{prompt_2}.png")
-    final_image_blend.save(f"{output_path}/{FINAL_IMAGE_BLEND_PREFIX}-{additional_parameters}-{prompt_1}-BLEND-{prompt_2}.png")
+    if additional_parameters is not None:
+        final_image_blend.save(f"{output_path}/{FINAL_IMAGE_BLEND_PREFIX}-{additional_parameters}-{prompt_1}-BLEND-{prompt_2}.png")
+    else:
+        final_image_blend.save(f"{output_path}/{FINAL_IMAGE_BLEND_PREFIX}-{prompt_1}-BLEND-{prompt_2}.png")
     
     make_animation(
         decoded_images=blend_images,
@@ -38,12 +40,14 @@ def save_all_outputs(config, prompt_1_images, prompt_2_images, blend_images, ble
         prompt_1 = prompt_1,
         prompt_2 = prompt_2,
         timesteps = timesteps,
+        blend_method=blend_method,
         additional_parameters = additional_parameters,
         output_path = output_path
     )
     
     
 def get_additional_parameters_string(config, blend_method):
+    additional_parameters_string =  None
     if blend_method == "blended_diffusion":
         from_timestep = config["from_timestep"]
         to_timestep = config["to_timestep"]
@@ -52,6 +56,7 @@ def get_additional_parameters_string(config, blend_method):
         pass
     elif blend_method == "blended_interpolated_prompts":
         interpolation_scale = config["blended_interpolated_prompts_scale"]
+        interpolation_scale = str(interpolation_scale).replace(".", "-")
         additional_parameters_string = f"scale_{interpolation_scale}"
     elif blend_method == "blended_alternate_unet":
         pass
@@ -62,6 +67,7 @@ def get_additional_parameters_string(config, blend_method):
 
 
 def make_animation(decoded_images: list, prompt: str, output_path: str):
+    plt.close()
     fig, ax = plt.subplots()
 
     plt.title(prompt[0])
@@ -83,22 +89,28 @@ def make_animation(decoded_images: list, prompt: str, output_path: str):
     ani.save(save_path)
     
     
-def make_plots(image_1, image_2, image_blend, prompt_1, prompt_2, timesteps, additional_parameters, output_path):
+def make_plots(image_1, image_2, image_blend, prompt_1, prompt_2, timesteps, blend_method, additional_parameters, output_path):
+    plt.close()
     fig, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].imshow(image_1)
     ax[0].set_title(prompt_1+f"\ntimesteps: {timesteps}")
     ax[0].axis("off")
     
     ax[1].imshow(image_blend)
-    ax[1].set_title(f"{prompt_1}-BLEND-{prompt_2}-{additional_parameters}")
+    if additional_parameters is not None:
+        ax[1].set_title(f"{prompt_1}-BLEND-{prompt_2}-{blend_method}-{additional_parameters}")
+    else:
+        ax[1].set_title(f"{prompt_1}-BLEND-{prompt_2}-{blend_method}")
     ax[1].axis("off")
     
     ax[2].imshow(image_2)
     ax[2].set_title(prompt_2+f"\ntimesteps: {timesteps}")
     ax[2].axis("off")
-    
-    plt.savefig(f"{output_path}/blending-{additional_parameters}-{prompt_1}-BLEND-{prompt_2}.png")
-    
+   
+    if additional_parameters is not None:
+        plt.savefig(f"{output_path}/blending-{blend_method}-{additional_parameters}-{prompt_1}-BLEND-{prompt_2}.png") 
+    else:
+        plt.savefig(f"{output_path}/blending-{blend_method}-{prompt_1}-BLEND-{prompt_2}.png")
     
     
 def make_blending_batch_grid(output_paths, blend_method, config):
@@ -111,14 +123,20 @@ def make_blending_batch_grid(output_paths, blend_method, config):
    
     rows = [] 
     for folder in output_paths:
-        image_1 = plt.imread(os.path.join(folder, f"final_image-{prompt_1}.png"))
-        image_2 = plt.imread(os.path.join(folder, f"final_image-{prompt_2}.png"))
-        blend_image = plt.imread(os.path.join(folder, f"final_image-{prompt_1}-BLEND-{prompt_2}.png"))
+        image_1 = plt.imread(os.path.join(folder, f"{FINAL_IMAGE_PREFIX}-{prompt_1}.png"))
+        image_2 = plt.imread(os.path.join(folder, f"{FINAL_IMAGE_PREFIX}-{prompt_2}.png"))
+        if additional_parameters is not None:
+            blend_image = plt.imread(os.path.join(folder, f"{FINAL_IMAGE_BLEND_PREFIX}-{additional_parameters}-{prompt_1}-BLEND-{prompt_2}.png"))
+        else:
+            blend_image = plt.imread(os.path.join(folder, f"{FINAL_IMAGE_BLEND_PREFIX}-{prompt_1}-BLEND-{prompt_2}.png"))
         rows.append([image_1, blend_image, image_2])
         
     plt.close()
     fig, axs = plt.subplots(len(rows), 3, figsize=(20, 32))
-    fig.suptitle(f'{prompt_1}-BLEND-{prompt_2}-{blend_method}-{additional_parameters}', fontsize=20)
+    if additional_parameters is not None:
+        fig.suptitle(f'{prompt_1}-BLEND-{prompt_2}-[{blend_method}-{additional_parameters}]', fontsize=20)
+    else:
+        fig.suptitle(f'{prompt_1}-BLEND-{prompt_2}-[{blend_method}]', fontsize=20)
     
     for i, row in enumerate(rows):
         axs[i, 0].set_title(f'prompt 1: {prompt_1}')
@@ -137,8 +155,9 @@ def make_blending_batch_grid(output_paths, blend_method, config):
     save_path = "./out"
     save_path = os.path.join(save_path, f"{prompt_1}-BLEND-{prompt_2}")
     save_path = os.path.join(save_path, blend_method)
-    save_path = os.path.join(save_path, additional_parameters)
-    save_path = os.path.join(save_path, f"results-[{blend_method}]-[{additional_parameters}]-[{model_id}]-[{scheduler_name}]")
-       
-    plt.savefig(save_path)
+    if additional_parameters is not None: 
+        save_path = os.path.join(save_path, additional_parameters)
+    save_path = os.path.join(save_path, f"results-[{blend_method}]-[{additional_parameters}]-[{model_id}]-[{scheduler_name}].png")
     
+    plt.savefig(save_path)
+       
--- src/utils.py
+++ src/utils.py
@@ -45,7 +45,7 @@ def make_output_dir(seed, config, blend_method, overwrite):
     output_path = os.path.join(output_path, f"{prompt_1}-BLEND-{prompt_2}")
     output_path = os.path.join(output_path, blend_method)
     if blend_method == "blended_diffusion":
-        output_path = os.path.join(output_path, f"[from_{from_timestep}]-[to_{to_timestep}]")
+        output_path = os.path.join(output_path, f"from_{from_timestep}-to_{to_timestep}")
         output_path = os.path.join(output_path, str(seed))
         output_path = os.path.join(output_path, f"[{blend_method}]-[from_{from_timestep}]-[to_{to_timestep}]-[{scheduler_name}]-[{model_id}]")  
     elif blend_method == "blended_in_unet":
@@ -53,6 +53,9 @@ def make_output_dir(seed, config, blend_method, overwrite):
         output_path = os.path.join(output_path, f"[{blend_method}]-[{scheduler_name}]-[{model_id}]-[p1_{timesteps}]-[p2_{timesteps}]")  
     elif blend_method == "blended_interpolated_prompts":
         interpolation_scale = config["blended_interpolated_prompts_scale"]
+        interpolation_scale = str(interpolation_scale).replace(".", "-")
+        additional_parameters_string = f"scale_{interpolation_scale}"
+        output_path = os.path.join(output_path, additional_parameters_string)
         output_path = os.path.join(output_path, str(interpolation_scale))
         output_path = os.path.join(output_path, str(seed))
         output_path = os.path.join(output_path, f"[{blend_method}]-[scale_{interpolation_scale}]-[{scheduler_name}]-[{model_id}]-[p1_{timesteps}]-[p2_{timesteps}]")
